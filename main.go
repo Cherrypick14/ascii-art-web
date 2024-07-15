970535d91d2e54684c4d0819dbf1b26fb17ec54f
Cherrypick14 <cherrypicklet@gmail.com>
2024-07-15 14:20:43 +0300

    [Fix] Change the file path import for handler

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"asciiweb/internal/handlers"
+	"asciiweb/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    // Handle the root path by serving the home page
-    http.HandleFunc("/", handlers.HomeHandler)
-    // Handle the /ascii-art path by processing the form submission
-    http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
+	// Handle the root path by serving the home page
+	http.HandleFunc("/", handlers.HomeHandler)
+	// Handle the /ascii-art path by processing the form submission
+	http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
 
-    // Start the server on port 8080
-    log.Println("Server started on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatalf("Could not start server: %s\n", err.Error())
-    }
+	// Start the server on port 8080
+	log.Println("Server started on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }
